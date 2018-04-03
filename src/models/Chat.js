3cbb217fdfc5b738e73e56568fd40068a0f8ad7f
workvictor <workvictor@yandex.ru>
2018-04-03 21:56:55 +0300

    refactor code

--- src/models/Chat.js
+++ src/models/Chat.js
@@ -1,9 +1,15 @@
-import mongoose         from 'mongoose';
+import mongoose from 'mongoose';
 
 
 const chatSchema=mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  messages: [{ text: String, date: Number, from: mongoose.Schema.Types.ObjectId}],
+  messages: [
+    {
+      text: String,
+      date: Number,
+      from: mongoose.Schema.Types.ObjectId
+    }
+  ],
   userIds: [Number],
 });
 export const Chat=mongoose.model(`Chat`, chatSchema);
